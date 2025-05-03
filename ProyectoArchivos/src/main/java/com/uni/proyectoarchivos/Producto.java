c6ef787b405657f3bf2966a2b71f5222b66e02f5
Josuke <ezrealmemb@gmail.com>
2025-05-03 01:47:25 -0600

    Hice mi proyecto de archivos

---
+++ ProyectoArchivos/src/main/java/com/uni/proyectoarchivos/Producto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uni.proyectoarchivos;
+
+/**
+ *
+ * @author rmuzu
+ */
+public class Producto {
+    
+    public int clave;
+    public String Nombre;
+    
+}
